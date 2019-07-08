69ca7bf7960650f7bab265de964671c7fde2c44b
pdhoward <patrick.howard@hotmail.com>
2019-07-08 15:00:50 -0400

    push class solutions

---
+++ 01/solved/script.js
@@ -0,0 +1,14 @@
+/*****************************
+* Variables and data types
+*/
+
+var firstName = 'Sally';
+console.log(firstName);
+var lastName = 'Jones';
+var age = 18;
+var fullAge = true;
+console.log(fullAge);
+var job;
+console.log(job);
+job = 'Road to Hire Student';
+console.log(job);
